dd5b1799ca95c1a50a5eccb084731f77a552819e
Trevor D. Miller <trevordmiller@icloud.com>
2016-09-20 14:12:21 -0600

    Polish typing effect

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,37 +3,66 @@ import Typist from 'react-typist';
 import about from '../data/about'
 import Heading from './Heading'
 
+const pauses = {
+  short: 60,
+  medium: 200,
+  long: 750,
+}
+
 const cursor = {
   show: true,
   blink: true,
   element: '|',
   hideWhenDone: true,
-  hideWhenDoneDelay: 0,
+  hideWhenDoneDelay: pauses.short,
 }
 
-const Intro = ({onIntroEnd}) => (
-  <div>
-    <Heading level={1}>
-      <Typist
-        cursor={cursor}
-        startDelay={500}
-        avgTypingDelay={200}
-      >
-        {about.title}
-      </Typist>
-    </Heading>
-    <Heading level={2}>
-      <Typist
-        cursor={cursor}
-        startDelay={1500}
-        avgTypingDelay={60}
-        onTypingDone={onIntroEnd}
-      >
-        {about.description}
-      </Typist>
-    </Heading>
-  </div>
-)
+class Intro extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasLogoEnded: false,
+    }
+    this.handleLogoEnd = this.handleLogoEnd.bind(this)
+  }
+
+  handleLogoEnd() {
+    this.setState({
+      hasLogoEnded: true,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Heading level={1}>
+          <Typist
+            cursor={cursor}
+            startDelay={pauses.long}
+            avgTypingDelay={pauses.medium}
+            onTypingDone={this.handleLogoEnd}
+          >
+            {about.title}
+          </Typist>
+        </Heading>
+        <Heading level={2}>
+          {this.state.hasLogoEnded
+            ? <Typist
+                cursor={cursor}
+                startDelay={pauses.medium}
+                avgTypingDelay={pauses.short}
+                onTypingDone={this.props.onIntroEnd}
+              >
+                {about.description}
+              </Typist>
+            : null
+          }
+        </Heading>
+      </div>
+    )
+  }
+}
 
 Intro.propTypes = {
   onIntroEnd: React.PropTypes.func.isRequired,
